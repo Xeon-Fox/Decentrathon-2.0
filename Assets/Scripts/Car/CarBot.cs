eb9fd8b5634a8c5d0be3beb19aa705ca93045fb6
rmxqwo <158151985+rmxqwo@users.noreply.github.com>
2024-10-19 01:12:15 +0600

    537

---
+++ Assets/Scripts/Car/CarBot.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBot : Car
+{
+    [SerializeField] float botTurnStrength = 5f;
+    [SerializeField] CarProgressHandler progressHandler;
+    [SerializeField] float baseSpeed = 1000f;  // базовая скорость
+    LapHandler lapHandler;
+    DifficultyChooser difficultyChooser;
+
+    public Vector3 Destination { get; set; }
+
+    void Start()
+    {
+        progressHandler.OnBotFinishedLap += HandleBotFinishedLap;
+        progressHandler.OnBotCrossedCheckpoint += HandleBotCrossedCheckpoint;
+        lapHandler = FindObjectOfType<LapHandler>();
+        difficultyChooser = FindObjectOfType<DifficultyChooser>(); // Получаем ссылку на DifficultyChooser
+
+        if (difficultyChooser == null)
+        {
+            Debug.LogError("DifficultyChooser не найден!");
+        }
+        else
+        {
+            // Установим сложность сразу при старте
+            AdjustSpeedAndTurnStrength();
+        }
+
+        Destination = lapHandler.Checkpoints[0].GetRandomDestination();
+    }
+
+    void OnDestroy()
+    {
+        progressHandler.OnBotFinishedLap -= HandleBotFinishedLap;
+        progressHandler.OnBotCrossedCheckpoint -= HandleBotCrossedCheckpoint;
+    }
+
+    void HandleBotFinishedLap(CarBot bot)
+    {
+        var checkpoints = lapHandler.Checkpoints;
+        Destination = checkpoints[0].GetRandomDestination();
+    }
+
+    void HandleBotCrossedCheckpoint(CarBot bot, int checkpointIndex)
+    {
+        var checkpoints = lapHandler.Checkpoints;
+        Destination = lapHandler.FinalCheckpoint == checkpointIndex ?
+            lapHandler.gameObject.transform.position :
+            checkpoints[checkpointIndex + 1].GetRandomDestination();
+    }
+
+    void LateUpdate()
+    {
+        BotMove();
+        base.LateUpdate();
+    }
+
+    void BotMove()
+    {
+        AdjustSpeedAndTurnStrength();  // Настройка скорости и силы поворота
+        Speed = ForwardAccel * baseSpeed;
+        if (GroundChecker.OnGround) State = CarState.OnGroundAndMovingForward;
+        else State = CarState.OffGround;
+        RotateBotTowardsDestination();
+    }
+
+    void AdjustSpeedAndTurnStrength()
+    {
+        // Проверяем наличие difficultyChooser
+        if (difficultyChooser != null)
+        {
+            switch (difficultyChooser.selectedDifficulty)
+            {
+                case 1: // Легкая сложность
+                    baseSpeed = 400f;  // Уменьшаем скорость
+                    botTurnStrength = 3f;  // Уменьшаем силу поворота
+                    break;
+                case 2: // Средняя сложность
+                    baseSpeed = 800f;  // Базовая скорость
+                    botTurnStrength = 5f;  // Базовая сила поворота
+                    break;
+                case 3: // Высокая сложность
+                    baseSpeed = 1200f;  // Увеличиваем скорость
+                    botTurnStrength = 7f;  // Увеличиваем силу поворота
+                    break;
+                default:
+                    Debug.LogWarning("Неизвестный уровень сложности: " + difficultyChooser.selectedDifficulty);
+                    break;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("DifficultyChooser не инициализирован.");
+        }
+    }
+
+    void RotateBotTowardsDestination()
+    {
+        Vector3 relativePos = Destination - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(relativePos);
+        float randomTurnStrength = Random.Range(botTurnStrength, botTurnStrength * 2);
+
+        Vector3 oldRotation = transform.rotation.eulerAngles;
+
+        if (GroundChecker.OnGround)
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                targetRotation,
+                Time.deltaTime * randomTurnStrength);
+
+        if ((oldRotation - transform.rotation.eulerAngles).y > 0.05f)
+            TurnInput = Mathf.Lerp(TurnInput, -1, Time.deltaTime * 10);
+        else if ((oldRotation - transform.rotation.eulerAngles).y < -0.05f)
+            TurnInput = Mathf.Lerp(TurnInput, 1, Time.deltaTime * 10);
+        else TurnInput = Mathf.Lerp(TurnInput, 0, Time.deltaTime * 10);
+    }
+}
